3e78f3ad087f318392285a82fa56819c30347cb6
SteveLasker <StevenLasker@Hotmail.com>
2014-09-28 22:14:02 -0700

    Completed updates for 0.5.4 and the new UX, including SpellCheck, Sample Code, Troubleshooting

--- scripts/starterpages.js
+++ scripts/starterpages.js
@@ -1,63 +1,68 @@
 // JavaScript Document
 
+function resetHorizontalNav(tab, items) {
+    LinkTableNavMVC.className = "linkTableNavItem";
+    LinkTableNavSnippets.className = "linkTableNavItem";
+    document.getElementById(tab).setAttribute('class', 'linkTableNavItemSelected');
 
-function resetLinkTableNav(tab, items)
-{
-	
-	LinkTableNavMVC.className = "linkTableNavItem";
-	LinkTableNavSnippets.className = "linkTableNavItem";
-	LinkTableNav3.className = "linkTableNavItem";
-	document.getElementById(tab).setAttribute('class', 'linkTableNavItemSelected');
-	
-	MVCLinks.style.display = "none";
-	Links2.style.display = "none";
-	Links3.style.display = "none";
-	document.getElementById(items).style.display = "inline";
-}
-	
-function resetMVCLinks(tab, items)
-{
-	MVCLinksSubNav1.className = "linkTableSubNavItem";
-	MVCLinksSubNav2.className = "linkTableSubNavItem";
-	MVCLinksSubNav3.className = "linkTableSubNavItem";
-	document.getElementById(tab).setAttribute('class', 'linkTableSubNavItemSelected');
-	
-	MVCLinksCode1.style.display = "none";
-	MVCLinksCode2.style.display = "none";
-	MVCLinksCode3.style.display = "none";
-	document.getElementById(items).style.display = "inline";
+    MVCLinks.style.display = "none";
+    SnippetLinks.style.display = "none";
+    document.getElementById(items).style.display = "inline";
 }
 
-function resetLinks2(tab, items)
-{
-	Links2SubNav1.className = "linkTableSubNavItem";
-	Links2SubNav2.className = "linkTableSubNavItem";
-	Links2SubNav3.className = "linkTableSubNavItem";
-	Links2SubNav4.className = "linkTableSubNavItem";
-	document.getElementById(tab).setAttribute('class', 'linkTableSubNavItemSelected');
-	
-	Links2Code1.style.display = "none";
-	Links2Code2.style.display = "none";
-	Links2Code3.style.display = "none";
-	Links2Code4.style.display = "none";
-	document.getElementById(items).style.display = "inline";
+function resetMVCLinks(tab, items) {
+    MVCLinksSubNavOverview.className = "linkTableSubNavItem";
+    MVCLinksSubNavController.className = "linkTableSubNavItem";
+    MVCLinksSubNavMenu.className = "linkTableSubNavItem";
+    MVCLinksSubNavIndex.className = "linkTableSubNavItem";
+    MVCLinksSubNavDetails.className = "linkTableSubNavItem";
+    MVCLinksSubNavEdit.className = "linkTableSubNavItem";
+    MVCLinksSubNavDelete.className = "linkTableSubNavItem";
+    MVCLinksSubNavCreate.className = "linkTableSubNavItem";
+    MVCLinksSubNavRunIt.className = "linkTableSubNavItem";
+    MVCLinksSubNavTroubleshooting.className = "linkTableSubNavItem";
+    document.getElementById(tab).setAttribute('class', 'linkTableSubNavItemSelected');
+
+    MVCLinksOverview.style.display = "none";
+    MVCLinksController.style.display = "none";
+    MVCLinksMenu.style.display = "none";
+    MVCLinksViewIndex.style.display = "none";
+    MVCLinksViewDetails.style.display = "none";
+    MVCLinksViewEdit.style.display = "none";
+    MVCLinksViewDelete.style.display = "none";
+    MVCLinksViewCreate.style.display = "none";
+    MVCLinksRunIt.style.display = "none";
+    MVCLinksTroubleshooting.style.display = "none";
+    document.getElementById(items).style.display = "inline";
 }
 
-function resetLinks3(tab, items)
-{
-	Links3SubNav1.className = "linkTableSubNavItem";
-	Links3SubNav2.className = "linkTableSubNavItem";
-	Links3SubNav3.className = "linkTableSubNavItem";
-	document.getElementById(tab).setAttribute('class', 'linkTableSubNavItemSelected');
-	
-	Links3Code1.style.display = "none";
-	Links3Code2.style.display = "none";
-	Links3Code3.style.display = "none";
-	document.getElementById(items).style.display = "inline";
+function resetSnippetLinks(tab, items) {
+    SnippetLinksSubNav1.className = "linkTableSubNavItem";
+    SnippetLinksSubNav2.className = "linkTableSubNavItem";
+    SnippetLinksSubNav3.className = "linkTableSubNavItem";
+    SnippetLinksSubNav4.className = "linkTableSubNavItem";
+    SnippetLinksSubNav5.className = "linkTableSubNavItem";
+    SnippetLinksSubNav6.className = "linkTableSubNavItem";
+    document.getElementById(tab).setAttribute('class', 'linkTableSubNavItemSelected');
+
+    SnippetLinksCode1.style.display = "none";
+    SnippetLinksCode2.style.display = "none";
+    SnippetLinksCode3.style.display = "none";
+    SnippetLinksCode4.style.display = "none";
+    SnippetLinksCode5.style.display = "none";
+    SnippetLinksCode6.style.display = "none";
+    document.getElementById(items).style.display = "inline";
 }
-function copyCode(id) {
-    if (window.clipboardData) {
-        var obj = document.getElementById(id);
-        window.clipboardData.setData("Text", obj.innerText);
-    }
+
+function resetLinks3(tab, items) {
+    Links3SubNav1.className = "linkTableSubNavItem";
+    Links3SubNav2.className = "linkTableSubNavItem";
+    Links3SubNav3.className = "linkTableSubNavItem";
+    document.getElementById(tab).setAttribute('class', 'linkTableSubNavItemSelected');
+
+    Links3Code1.style.display = "none";
+    Links3Code2.style.display = "none";
+    Links3Code3.style.display = "none";
+    document.getElementById(items).style.display = "inline";
 }
+
